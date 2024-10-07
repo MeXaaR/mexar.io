352a220bbcc6e6667746a823a7a3cf89c0792bf1
Francois AUBEUT <francois.aubeut-gervaise@carrier.com>
2024-10-07 13:48:27 +0200

    add some config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    publicRuntimeConfig: {
+    }
+};
 
-export default nextConfig;
+module.exports = nextConfig;
